fc13c39790340ed7e736a235df0ba713618c682e
Andreas Dinauer <andreas.j.dinauer@gmail.com>
2024-04-11 14:15:07 +0200

    changes to import.sql and data.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,5 @@
-DROP TABLE IF EXISTS event;
-DROP TABLE IF EXISTS address;
-
+DROP SCHEMA benevolo CASCADE;
+CREATE SCHEMA benevolo;
 
 CREATE TABLE address (
      id VARCHAR(255),
@@ -9,9 +8,7 @@ CREATE TABLE address (
      city VARCHAR(255),
      zip VARCHAR(255),
      country VARCHAR(255),
-     event_id VARCHAR(255),
-     CONSTRAINT pk_address PRIMARY KEY (id),
-     CONSTRAINT fk_address_event FOREIGN KEY (event_id) REFERENCES event(id)
+     CONSTRAINT pk_address PRIMARY KEY (id)
 );
 
 CREATE TABLE event (
@@ -25,4 +22,36 @@ CREATE TABLE event (
     CONSTRAINT fk_event_address FOREIGN KEY (address_id) REFERENCES address(id)
 );
 
+CREATE TABLE ticket_type (
+    id VARCHAR(256),
+    ticket_type_name VARCHAR(256),
+    price INTEGER,
+    tax_rate INTEGER,
+    valid_from TIMESTAMP,
+    valid_to TIMESTAMP,
+    capacity INTEGER,
+    active BOOLEAN,
+    event_id VARCHAR(256),
+    CONSTRAINT pk_ticket_type PRIMARY KEY (id),
+    CONSTRAINT fk_event FOREIGN KEY (event_id) REFERENCES event(id)
+);
+
+CREATE TABLE customer (
+    stripe_id VARCHAR(256),
+    customer_name VARCHAR(256),
+    email VARCHAR(256),
+    CONSTRAINT pk_customer PRIMARY KEY (stripe_id)
+);
+
+CREATE TABLE ticket (
+    id VARCHAR(256),
+    status VARCHAR(32),
+    price INTEGER,
+    tax_rate INTEGER,
+    customer_id VARCHAR(256),
+    ticket_type_id VARCHAR(256),
+    CONSTRAINT pk_ticket PRIMARY KEY (id),
+    CONSTRAINT fk_customer FOREIGN KEY (customer_id) REFERENCES customer(stripe_id),
+    CONSTRAINT fk_ticket_type FOREIGN KEY (ticket_type_id) REFERENCES ticket_type(id)
+);
 
